97d7d63198e9dae657e648269fe2d11f8641d12d
mandu <jys0972@gmail.com>
2024-10-01 14:24:24 +0900

    feat(Header.js) : home href 연결

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ function Header() {
         <img className="logo" src="drug-master-logo.png"></img>
         <nav className="nav-menu">
             <ul>
-            <li><a href="#home">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#contact">Contact</a></li>
