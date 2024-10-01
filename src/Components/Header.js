7d7630991ea6935e3d21cf95be96b1c440cc9728
mandu <jys0972@gmail.com>
2024-10-01 14:04:17 +0900

    feat(Header.js) : 헤더 컴포넌트 구현

---
+++ src/Components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+        <div className='container'>
+        <img className="logo" src="drug-master-logo.png"></img>
+        <nav className="nav-menu">
+            <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
