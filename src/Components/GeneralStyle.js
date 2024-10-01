371bf1ec608efaa058872543f3d90bfc412e08fa
mandu <jys0972@gmail.com>
2024-10-01 14:15:27 +0900

    feat(GeneralStyle.js) : MainContainer style 정의

---
+++ src/Components/GeneralStyle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #FFFCF5;
+    min-height: 100vh;
+    position: relative;
+    padding: 20px;
+`;
