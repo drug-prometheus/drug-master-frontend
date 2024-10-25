8d07c11ba43fa95e1849fcc5c5722f1d3a42dda4
mandu <jys0972@gmail.com>
2024-10-25 15:37:16 +0900

    refact(Header.js)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,13 +7,13 @@ function Header() {
   return (
     <header className="header">
         <div className='container'>
-        <img className="logo" src="drug-master-logo.png" onClick={() => navigate('/')}></img>
-        <nav className="nav-menu">
-            <ul>
-            <li><a href="/">메인 화면</a></li>
-            <li><a href="/introduction">다제약물 관리사업 소개</a></li>
-            </ul>
-        </nav>
+          <img className="logo" src="drug-master-logo.png" onClick={() => navigate('/')}></img>
+          <nav className="nav-menu">
+              <ul>
+                <li><a href="/">메인 화면</a></li>
+                <li><a href="/introduction">다제약물 관리사업 소개</a></li>
+              </ul>
+          </nav>
         </div>
     </header>
   );
